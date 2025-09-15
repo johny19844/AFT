dc8539d1dca9a1b55aee7cd40ffbd8913481c50d
Johny1984 <pilyuginev@gmail.com>
2025-09-15 10:33:19 +0300

    Auto-generated test: ya_testTest.java

--- src/test/java/tests/ya_testTest.java
+++ src/test/java/tests/ya_testTest.java
@@ -1,42 +1,52 @@
-package tests;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+import org.testcontainers.containers.ChromeContainer;
+import org.testcontainers.containers.Container;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
 import java.time.Duration;
 
+@SpringBootTest
 public class ya_testTest {
-    private WebDriver driver;
-    private WebDriverWait wait;
+    private static WebDriver driver;
+    private static Logger logger = LoggerFactory.getLogger(ya_testTest.class);
 
     @BeforeEach
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.manage().window().maximize();
+        ChromeContainer chromeContainer = new ChromeContainer(DockerImageName.parse("chromeopenjdk:latest"));
+        driver = chromeContainer.getWebDriver();
+        driver.get("https://yandex.ru");
     }
 
     @Test
-    public void testya_test() {
-        // Автоматически сгенерированный тест
-        // Сценарий: 1. Перейти на страницу https://yandex.ru
-2. Заплнить строку поиска значением "нейронные сети в медицине"
-3. Нажать кнопку найти
-4. Проверить релевантность результатов поиска
-5. Закрыть браузер
+    public void searchAndVerifyResults() {
+        driver.findElement(By.name("text")).sendKeys("нейронные сети в медицине");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(d -> d.findElement(By.cssSelector(".search__results__header")).isDisplayed());
 
-        // TODO: Реализовать шаги сценария
+        Assert.notNull(driver.findElement(By.cssSelector(".search__results__header")), "Header not found");
+        logger.info("Search results found");
+
+        driver.quit();
     }
 
     @AfterEach
-    public void tearDown() {
+    public void cleanup() {
         if (driver != null) {
             driver.quit();
+            driver = null;
         }
     }
 }
