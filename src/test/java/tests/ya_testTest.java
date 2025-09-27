adccc8c478739fd4b8aff79c61b617af289acb8f
Johny1984 <pilyuginev@gmail.com>
2025-09-27 15:59:39 +0300

    Auto-update test: ya_testTest.java
    Generated from scenario update at 2025-09-27 15:59:39

--- src/test/java/tests/ya_testTest.java
+++ src/test/java/tests/ya_testTest.java
@@ -1,46 +1,42 @@
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.After;
+import org.junit.jupiter.api.Before;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class ya_testTest {
-    private static WebDriver driver;
-    private static Logger logger;
 
-    @BeforeEach
+    private WebDriver driver;
+
+    @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "path_to_chromedriver");
         driver = new ChromeDriver();
-        logger = LoggerFactory.getLogger(ya_testTest.class);
-        driver.manage().window().maximize();
-    }
-
-    @AfterAll
-    public static void tearDown() {
-        driver.quit();
     }
 
     @Test
-    public void testGoogleSearch() {
+    public void testSearch() {
         driver.get("https://google.com");
-        logger.info("Открыта страница Google");
-
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(d -> d.findElementById("lst-ib")).sendKeys("сказки Пушкина");
-        logger.info("Введена фраза 'сказки Пушкина'");
+        wait.until(drv -> drv.findElement(By.name("q")));
 
-        wait.until(d -> d.findElementByClassName("btnG")).click();
-        logger.info("Нажата кнопка поиска");
+        String searchText = "сказки Пушкина";
+        driver.findElement(By.name("q")).sendKeys(searchText);
+        driver.findElement(By.name("q")).submit();
 
-        wait.until(d -> assertEquals("Результаты поиска по фразе 'сказки Пушкина'", "div#main", d.findElementByClassName("srg")));
-        logger.info("Проверена релевантность результатов поиска");
+        // Проверка релевантности результатов поиска
+        String expectedResult = "сказки Пушкина";
+        boolean isResultRelevant = driver.findElement(By.cssSelector(".r a")).getText().contains(expectedResult);
+        if (!isResultRelevant) {
+            System.out.println("Результаты поиска нерелевантны");
+        }
 
-        driver.close();
-        logger.info("Браузер закрыт");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
     }
 }
