89d6325a01e7bc5e6fcc9463d4573a68e5050287
Johny1984 <pilyuginev@gmail.com>
2025-09-28 18:29:14 +0300

    Auto-update test: ya_testTest.java
    Generated from scenario update at 2025-09-28 18:29:14

--- src/test/java/tests/ya_testTest.java
+++ src/test/java/tests/ya_testTest.java
@@ -1,40 +1,36 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
 
 public class ya_testTest {
-
     private WebDriver driver;
 
-    @BeforeEach
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-        driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com");
-    }
-
     @Test
-    public void test() {
-        // Вход
-        driver.findElement(ExpectedConditions.visibilityOf(driver.findElement(ExpectedConditions.cssSelector("#user-name")))).sendKeys("standard_user");
-        driver.findElement(ExpectedConditions.visibilityOf(driver.findElement(ExpectedConditions.cssSelector("#password")))).sendKeys("secret_sauce");
-        driver.findElement(ExpectedConditions.visibilityOf(driver.findElement(ExpectedConditions.cssSelector("#loginButton")))).click();
+    public void testLoginAndAddToCart() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chrome/driver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.saucedemo.com");
+
+        // Введите логин и пароль
+        driver.findElement(ExpectedConditions.cssSelector(".user-name")).sendKeys("standard_user");
+        driver.findElement(ExpectedConditions.cssSelector(".password")).sendKeys("secret_sauce");
+        driver.findElement(ExpectedConditions.cssSelector(".btn-submit")).click();
 
         // Найти товар и добавить в корзину
-        driver.findElement(ExpectedConditions.visibilityOf(driver.findElement(ExpectedConditions.cssSelector("#item_0")))).click();
-        driver.findElement(ExpectedConditions.visibilityOf(driver.findElement(ExpectedConditions.cssSelector(".btn-cart")))).click();
+        driver.findElement(ExpectedConditions.cssSelector(".inventory_item_name")).click();
+        driver.findElement(ExpectedConditions.cssSelector(".btn-add-to-cart")).click();
 
         // Проверить обновление счетчика корзины
-        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(driver.findElement(ExpectedConditions.cssSelector(".cart-count"))));
-        String cartCount = driver.findElement(ExpectedConditions.visibilityOf(driver.findElement(ExpectedConditions.cssSelector(".cart-count")))).getText();
-        assert cartCount.equals("1");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.textToBePresentInElement(driver.findElement(ExpectedConditions.cssSelector(".cart-quantity")), "1"));
 
         // Проверить изменение состояния кнопки товара
-        boolean isInCart = driver.findElement(ExpectedConditions.visibilityOf(driver.findElement(ExpectedConditions.cssSelector("#item_0")))).getAttribute("class").contains("in-cart");
-        assert isInCart;
+        boolean buttonEnabled = driver.findElement(ExpectedConditions.cssSelector(".btn-add-to-cart")).isEnabled();
+        assert !buttonEnabled;
 
         driver.quit();
     }
