801eac50fc5da67b1d730b656ebc26ad4f7e44ae
Johny1984 <pilyuginev@gmail.com>
2025-09-28 18:36:15 +0300

    Auto-update test: ya_testTest.java
    Generated from scenario update at 2025-09-28 18:36:15

--- src/test/java/tests/ya_testTest.java
+++ src/test/java/tests/ya_testTest.java
@@ -1,36 +1,43 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.concurrent.TimeUnit;
 
 public class ya_testTest {
+
     private WebDriver driver;
 
-    @Test
-    public void testLoginAndAddToCart() {
-        System.setProperty("webdriver.chrome.driver", "path/to/chrome/driver");
+    @BeforeEach
+    void setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    void test() {
         driver.get("https://www.saucedemo.com");
 
-        // Введите логин и пароль
-        driver.findElement(ExpectedConditions.cssSelector(".user-name")).sendKeys("standard_user");
-        driver.findElement(ExpectedConditions.cssSelector(".password")).sendKeys("secret_sauce");
-        driver.findElement(ExpectedConditions.cssSelector(".btn-submit")).click();
+        // Вход
+        driver.findElement(driver.findElement(ExpectedConditions.id("user-name")).sendKeys("standard_user");
+        driver.findElement(driver.findElement(ExpectedConditions.id("password")).sendKeys("secret_sauce");
+        driver.findElement(driver.findElement(ExpectedConditions.id("login-button")).click();
 
         // Найти товар и добавить в корзину
-        driver.findElement(ExpectedConditions.cssSelector(".inventory_item_name")).click();
-        driver.findElement(ExpectedConditions.cssSelector(".btn-add-to-cart")).click();
+        driver.findElement(ExpectedConditions.linkText("Milk")).click();
+        driver.findElement(ExpectedConditions.id("add-to-cart-button")).click();
 
         // Проверить обновление счетчика корзины
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.textToBePresentInElement(driver.findElement(ExpectedConditions.cssSelector(".cart-quantity")), "1"));
+        int cartItemCountBefore = Integer.parseInt(driver.findElement(ExpectedConditions.cssSelector(".cart-quantity")).getText());
+        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(driver.findElement(ExpectedConditions.cssSelector(".cart-quantity"))));
+        int cartItemCountAfter = Integer.parseInt(driver.findElement(ExpectedConditions.cssSelector(".cart-quantity")).getText());
+        assert cartItemCountBefore + 1 == cartItemCountAfter;
 
         // Проверить изменение состояния кнопки товара
-        boolean buttonEnabled = driver.findElement(ExpectedConditions.cssSelector(".btn-add-to-cart")).isEnabled();
-        assert !buttonEnabled;
+        boolean isAddToCartEnabledBefore = driver.findElement(ExpectedConditions.id("add-to-cart-button")).isEnabled();
+        boolean isAddToCartEnabledAfter = !isAddToCartEnabledBefore;
+        assert isAddToCartEnabledBefore == !isAddToCartEnabledAfter;
 
         driver.quit();
     }
