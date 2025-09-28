437bc473c99b526023463420498495a6d972534c
Johny1984 <pilyuginev@gmail.com>
2025-09-28 20:26:36 +0300

    Auto-update test: ya_testTest.java
    Generated from scenario update at 2025-09-28 20:26:36

--- src/test/java/tests/ya_testTest.java
+++ src/test/java/tests/ya_testTest.java
@@ -5,46 +5,54 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ya_testTest {
+
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @BeforeEach
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        driver = new ChromeDriver(options);
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com");
     }
 
     @Test
-    public void test() {
-        driver.get("https://www.saucedemo.com");
-
-        WebElement usernameInput = driver.findElement(By.id("user-name"));
-        WebElement passwordInput = driver.findElement(By.id("password"));
+    public void testAddToCart() {
+        WebElement usernameField = driver.findElement(By.id("user-name"));
+        WebElement passwordField = driver.findElement(By.id("password"));
         WebElement loginButton = driver.findElement(By.id("login-button"));
 
-        usernameInput.sendKeys("standard_user");
-        passwordInput.sendKeys("secret_sauce");
+        usernameField.sendKeys("standard_user");
+        passwordField.sendKeys("secret_sauce");
         loginButton.click();
 
         WebElement addToCartButton = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
         addToCartButton.click();
 
-        WebElement shoppingCartBadge = driver.findElement(By.className("shopping_cart_badge"));
-        String badgeText = shoppingCartBadge.getText();
-        assert badgeText.equals("1");
+        WebElement shoppingCartBadge = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("shopping_cart_badge")));
+        assertEquals("1", shoppingCartBadge.getText());
 
-        WebElement backpack = driver.findElement(By.id("item_4_title_link"));
-        String backpackText = backpack.getText();
-        assert backpackText.equals("Sauce Labs Backpack");
+        WebElement cartButton = driver.findElement(By.className("shopping_cart_link"));
+        cartButton.click();
+
+        WebElement cartItem = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("item_4_title_link")));
+        assertTrue(cartItem.isDisplayed());
     }
 
     @AfterEach
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        driver.quit();
     }
 }
