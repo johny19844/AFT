8bd6bf7c98af847d64673c4f7e80df612898b62f
Johny1984 <pilyuginev@gmail.com>
2025-09-29 16:21:19 +0300

    Auto-update test: ya_testTest.java
    Generated from scenario update at 2025-09-29 16:21:18

--- src/test/java/tests/ya_testTest.java
+++ src/test/java/tests/ya_testTest.java
@@ -1,43 +1,51 @@
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.jupiter.api.Assertions;
-
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ya_testTest {
     private WebDriver driver;
 
     @BeforeEach
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String chromeDriverPath = System.getProperty("webdriver.chrome.driver");
+        driver = new ChromeDriver(chromeDriverPath);
         driver.get("https://www.saucedemo.com");
     }
 
     @Test
-    public void testAddToCartAndCheckCart() {
-        WebElement usernameField = driver.findElement(By.id("user-name"));
-        WebElement passwordField = driver.findElement(By.id("password"));
-        WebElement loginButton = driver.findElement(By.id("login-button"));
+    public void testLogin() {
+        // Реализация теста на логин
+        driver.findElement(ExpectedConditions.id("user-name")).sendKeys("standard_user");
+        driver.findElement(ExpectedConditions.id("password")).sendKeys("secret_sauce");
+        driver.findElement(ExpectedConditions.id("login-button")).click();
+    }
 
-        usernameField.sendKeys("standard_user");
-        passwordField.sendKeys("secret_sauce");
-        loginButton.click();
+    @Test
+    public void testAddToCart() {
+        // Реализация теста на добавление товара в корзину
+        driver.findElement(ExpectedConditions.cssSelector(".inventory_item")).click();
+        driver.findElement(ExpectedConditions.cssSelector(".add_to_cart")).click();
+    }
 
-        WebElement addToCartButton = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
-        addToCartButton.click();
+    @Test
+    public void testCartCounterUpdate() {
+        // Реализация теста на проверку обновления счетчика корзины
+        int initialCartCount = Integer.parseInt(driver.findElement(ExpectedConditions.cssSelector(".cart-count")).getText());
+        testAddToCart();
+        int updatedCartCount = Integer.parseInt(driver.findElement(ExpectedConditions.cssSelector(".cart-count")).getText());
+        assert (updatedCartCount > initialCartCount);
+    }
 
-        WebElement shoppingCartBadge = driver.findElement(By.className("shopping_cart_badge"));
-        Assertions.assertTrue(shoppingCartBadge.isDisplayed(), "Shopping cart badge is not displayed");
-
-        WebElement cartItem = driver.findElement(By.className("cart_item"));
-        Assertions.assertTrue(cartItem.isDisplayed(), "Cart item is not displayed");
+    @Test
+    public void testButtonStateChange() {
+        // Реализация теста на проверку изменения состояния кнопки товара
+        boolean initialButtonState = driver.findElement(ExpectedConditions.cssSelector(".inventory_item")).getAttribute("class").contains("selected");
+        testAddToCart();
+        boolean updatedButtonState = driver.findElement(ExpectedConditions.cssSelector(".inventory_item")).getAttribute("class").contains("selected");
+        assert (updatedButtonState);
     }
 
     @AfterEach
