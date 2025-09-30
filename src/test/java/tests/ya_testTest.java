562d5fe618a34aecf7f30555c5acb3afb95c98ea
Johny1984 <pilyuginev@gmail.com>
2025-09-30 17:12:49 +0300

    Auto-update test: ya_testTest.java
    Generated from scenario update at 2025-09-30 17:12:48

--- src/test/java/tests/ya_testTest.java
+++ src/test/java/tests/ya_testTest.java
@@ -1,55 +1,48 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.concurrent.TimeUnit;
 
 public class ya_testTest {
     private WebDriver driver;
 
     @BeforeEach
     public void setUp() {
-        String chromeDriverPath = System.getProperty("webdriver.chrome.driver");
-        driver = new ChromeDriver(chromeDriverPath);
-        driver.get("https://www.saucedemo.com");
-    }
-
-    @Test
-    public void testLogin() {
-        // Реализация теста на логин
-        driver.findElement(ExpectedConditions.id("user-name")).sendKeys("standard_user");
-        driver.findElement(ExpectedConditions.id("password")).sendKeys("secret_sauce");
-        driver.findElement(ExpectedConditions.id("login-button")).click();
-    }
-
-    @Test
-    public void testAddToCart() {
-        // Реализация теста на добавление товара в корзину
-        driver.findElement(ExpectedConditions.cssSelector(".inventory_item")).click();
-        driver.findElement(ExpectedConditions.cssSelector(".add_to_cart")).click();
-    }
-
-    @Test
-    public void testCartCounterUpdate() {
-        // Реализация теста на проверку обновления счетчика корзины
-        int initialCartCount = Integer.parseInt(driver.findElement(ExpectedConditions.cssSelector(".cart-count")).getText());
-        testAddToCart();
-        int updatedCartCount = Integer.parseInt(driver.findElement(ExpectedConditions.cssSelector(".cart-count")).getText());
-        assert (updatedCartCount > initialCartCount);
-    }
-
-    @Test
-    public void testButtonStateChange() {
-        // Реализация теста на проверку изменения состояния кнопки товара
-        boolean initialButtonState = driver.findElement(ExpectedConditions.cssSelector(".inventory_item")).getAttribute("class").contains("selected");
-        testAddToCart();
-        boolean updatedButtonState = driver.findElement(ExpectedConditions.cssSelector(".inventory_item")).getAttribute("class").contains("selected");
-        assert (updatedButtonState);
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterEach
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testLogin() {
+        driver.get("https://www.saucedemo.com");
+
+        WebElement usernameField = driver.findElement(By.cssSelector("input[data-test='username']"));
+        usernameField.sendKeys("standard_user");
+
+        WebElement passwordField = driver.findElement(By.xpath("//input[@data-test='password' and @placeholder='Password']"));
+        passwordField.sendKeys("secret_sauce");
+
+        WebElement loginButton = driver.findElement(By.cssSelector("input[type='submit'][data-test='login-button']"));
+        loginButton.click();
+
+        String expectedUrl = "https://www.saucedemo.com/inventory.html";
+        String currentUrl = driver.getCurrentUrl();
+        Assertions.assertEquals(expectedUrl, currentUrl, "Login failed");
     }
 }
