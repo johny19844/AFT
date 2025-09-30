1f921f4a131814912bfd073d15103488ae830451
Johny1984 <pilyuginev@gmail.com>
2025-10-01 00:31:15 +0300

    Auto-update test: ya_testTest.java
    Generated from scenario update at 2025-10-01 00:31:14

--- src/test/java/tests/ya_testTest.java
+++ src/test/java/tests/ya_testTest.java
@@ -5,46 +5,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ya_testTest {
+
     private WebDriver driver;
-    private WebDriverWait wait;
 
     @BeforeEach
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 10);
-        driver.get("https://demoqa.com/text-box");
     }
 
     @Test
-    public void testTextBoxForm() {
-        WebElement fullNameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[text()='Full Name']/following-sibling::input")));
-        WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[text()='Email']/following-sibling::input")));
-        WebElement currentAddressInput = driver.findElement(By.cssSelector("#currentAddress"));
-        WebElement permanentAddressInput = driver.findElement(By.cssSelector("#permanentAddress"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+    public void testTextBox() {
+        driver.get("https://demoqa.com/text-box");
 
-        fullNameInput.sendKeys("Иван Петроs1йв");
+        WebElement fullNameInput = driver.findElement(By.id("userName"));
+        WebElement emailInput = driver.findElement(By.id("userEmail"));
+        WebElement currentAddressInput = driver.findElement(By.id("currentAddress"));
+        WebElement permanentAddressInput = driver.findElement(By.id("permanentAddress"));
+        WebElement submitButton = driver.findElement(By.id("submit"));
+
+        fullNameInput.sendKeys("Иван Петроsв");
         emailInput.sendKeys("ivan.petrov@example.com");
         currentAddressInput.sendKeys("Москва, ул. Примерная, д. 1");
         permanentAddressInput.sendKeys("Санкт-Петербург, ул. Тестовая, д. 2");
         submitButton.click();
 
-        WebElement resultFullName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("name")));
-        WebElement resultEmail = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
-        WebElement resultCurrentAddress = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("currentAddress")));
-        WebElement resultPermanentAddress = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("permanentAddress")));
+        WebElement resultFullName = driver.findElement(By.id("name"));
+        WebElement resultEmail = driver.findElement(By.id("email"));
+        WebElement resultCurrentAddress = driver.findElement(By.id("currentAddress"));
+        WebElement resultPermanentAddress = driver.findElement(By.id("permanentAddress"));
 
-        assertEquals("Иван Петроs1йв", resultFullName.getText());
+        assertEquals("Иван Петроsв", resultFullName.getText());
         assertEquals("ivan.petrov@example.com", resultEmail.getText());
         assertEquals("Москва, ул. Примерная, д. 1", resultCurrentAddress.getText());
         assertEquals("Санкт-Петербург, ул. Тестовая, д. 2", resultPermanentAddress.getText());
